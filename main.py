bcfbe98bc365842b60258c0e863794743b565945
dalton-omens <daltonomens@gmail.com>
2016-09-24 11:31:16 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from math import pi
 
 try:
     radius = float(input('what is the radius?'))
-    print('The radius is ' + str(pi * radius ** 2))
+    print('The area is ' + str(pi * radius ** 2))
 
 except:
     print('you fool, that isn\'t a number')
