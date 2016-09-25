7204f702e3b14a27a2cc8822dcfac30e4342ea83
averyyip <averyyip@ymail.com>
2016-09-24 17:08:43 -0700

    Add files via upload

--- interest_suggestions.py
+++ interest_suggestions.py
@@ -9,16 +9,19 @@ def common_interests(genre_list, preferences):
 	return shared_interest
 
 def suggestions(genre_list, shared_interest):
+	suggested_site = []
 	if genre_list[-1] == 'Music':
 		suggested_site = 'https://play.spotify.com/search/' + shared_interest
-	elif genre_list[-1] == 'Movie':
+	elif genre_list[-1] == 'Movies':
 		suggested_site = 'http://www.metacritic.com/browse/movies/genre/metascore/' + shared_interest
-	elif genre_list[-1] == 'Game':
+	elif genre_list[-1] == 'Games':
 		suggested_site = 'http://www.metacritic.com/browse/games/genre/metascore/' + shared_interest
-	elif genre_list[-1] == 'Restaurant':
+	elif genre_list[-1] == 'Restaurants':
 		city = input('What city are you in?\n')
 		state = input('What state are you in? Use state abbreviations only: (Ex.CA)\n')
 		suggested_site = 'https://www.yelp.com/search?find_desc=' + shared_interest + '&find_loc=' + city + ',' + state + shared_interest
+
+	print(shared_interest, suggested_site)
 	suggested_print = ('For ' + shared_interest + ' suggestions, Go to ' + suggested_site)
 	print(suggested_print)
 	webbrowser.open(suggested_site)
