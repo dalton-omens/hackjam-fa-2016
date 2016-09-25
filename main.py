76cf22098ec4417564da024f37de3aece54f1ec3
averyyip <averyyip@ymail.com>
2016-09-24 18:47:22 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,14 @@ from get_genre_list import get_genre_list
 from interest_suggestions import common_interests, suggestions
 from make_preferences import make_preferences
 
-num_users = input('\nHow many of you are there?\n')
+num_users = input('\nHow many of you are there? Please enter a number.\n')
+correct = False
+while not correct:
+  try:
+    int(num_users)
+    correct = True
+  except NameError:
+    num_users = input('Please input a number. (Ex.4)')
 genre_list = get_genre_list()
 preferences = make_preferences(genre_list, num_users)
 shared_interest = common_interests(genre_list, preferences) #replace test with list of each user's preferences
