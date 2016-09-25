cec3a581a339afbd4ac5fcb674a942baeb14f296
dalton-omens <daltonomens@gmail.com>
2016-09-24 18:17:05 -0700

    Add files via upload

--- make_preferences.py
+++ make_preferences.py
@@ -1,6 +1,6 @@
 def make_preferences(genre_list, num_users):
-    def prompt():
-        print('What are you interested in? Here are your options: If you\'re done, type \"Done\".')
+    def prompt(_else):
+        print('\nWhat' + _else + 'are you interested in? Here are your options: If you\'re done, type \"Done\"\n.')
         lst = ''
         for i in range(len(genre_list) - 2):
             lst = lst + genre_list[i] + ', '
@@ -8,17 +8,19 @@ def make_preferences(genre_list, num_users):
         return input(lst).strip()
     preferences = []
     for user in range(int(num_users)):
+        print()
+        _else = ''
         print('User ' + str(user + 1) + ':')
         user_preferences = []
         done = False
         while not done:
-            new_preference = prompt()
+            new_preference = prompt(_else)
             if new_preference in genre_list and new_preference not in genre_list[:-3:-1] and new_preference not in user_preferences:
                 user_preferences += [new_preference]
             elif new_preference == 'Done' or new_preference == "done":
                 done = True
             else:
-                print('Please type a valid new word in the list.')
+                print('\nPlease type a valid new word in the list.')
+            _else = ' else '
         preferences += [user_preferences]
     return preferences
-
